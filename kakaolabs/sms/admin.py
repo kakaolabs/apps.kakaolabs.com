b45d3ced262946c53c34825fe45cfc34a0a77245
kiennt <trungkien2288@gmail.com>
2014-01-23 11:18:18 +0700

    Change admin

--- kakaolabs/sms/admin.py
+++ kakaolabs/sms/admin.py
@@ -7,14 +7,14 @@ from kakaolabs.sms import models
 
 class CategoryAdmin(admin.ModelAdmin):
     raw_id_fields = ('parent', )
-    list_display = ('parent', 'name', 'type')
+    list_display = ('name', 'type', 'parent', 'index')
     search_fields = ('name', )
     list_filter = ('type', )
 
 
 class SMSContentAdmin(admin.ModelAdmin):
     raw_id_fields = ('category', )
-    list_display = ('category', 'content', 'votes')
+    list_display = ('category', 'content', 'index', 'votes')
     list_filter = ('category', )
 
 
